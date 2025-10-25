02720f46d1fc2d72e6350e4e6803019dfcd6cd49
Tomas Martinkus <tomreikalai111@gmail.com>
2025-10-25 20:46:25 +0300

    fixing dogshit Juozas merge

--- CallyAIbackend/src/main/java/com/example/callyaibackend/CallyAIbackendApplication.java
+++ CallyAIbackend/src/main/java/com/example/callyaibackend/CallyAIbackendApplication.java
@@ -2,11 +2,14 @@ package com.example.callyaibackend;
 
 import com.example.callyaibackend.model.FoodItem;
 import com.example.callyaibackend.service.FoodApiService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Scanner;
-
+@SpringBootApplication
 public class CallyAIbackendApplication {
     public static void main(String[] args) {
+        SpringApplication.run(CallyAIbackendApplication.class, args);
         Scanner scanner = new Scanner(System.in);
         FoodApiService api = new FoodApiService();
 
